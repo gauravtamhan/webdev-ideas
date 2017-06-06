b84bcc7b079690bf43f38c1446f436761e8e5b51
Gaurav Tamhan <Gaurav.Tamhan@jda.com>
2017-06-06 15:21:37 -0400

    Added modal button

--- js/animate.js
+++ js/animate.js
@@ -1,6 +1,11 @@
  $(document).ready(function() {
  	$('.parallax').parallax();
  	$('.carousel').carousel();
+ 	$('.modal').modal({
+ 		dismissible: true, // Modal can be dismissed by clicking outside of the modal
+ 		startingTop: '10%', // Starting top style attribute
+      	endingTop: '10%' // Ending top style attribute
+ 	});
 
  	var options = [
     {selector: '#mylist', offset: 300, callback: function(el) {
